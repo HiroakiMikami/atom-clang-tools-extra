eaef64017b3bf13f4aab9b1518b364aaad889355
Hiroaki Mikami <hiroaki8270@gmail.com>
2016-10-21 09:44:22 +0900

    Fix clang-tidy-parser
      ref #2

--- spec/clang-tidy-parser-spec.js
+++ spec/clang-tidy-parser-spec.js
@@ -77,4 +77,28 @@ describe('clang-tidy parser', () => {
     expect(error2.excerpt).toBe("cannot initialize return object of type 'int' with an lvalue of type 'const char [1]' [clang-diagnostic-error]")
     expect(error2.trace.length).toBe(0)
   })
+  it('parses suggested fixes from the clang-tidy-output', () => {
+    const result = parse(fs.readFileSync('./spec/output/output5.txt', 'utf8'))
+    expect(result.length).toBe(3)
+
+    const [error1, error2, error3] = result
+
+    expect(error1.begin.row).toBe(2)
+    expect(error1.begin.column).toBe(5)
+    expect(error1.severity).toBe('error')
+    expect(error1.excerpt).toBe("unknown type name 'Foo' [clang-diagnostic-error]")
+    expect(error1.hasFix).not.toBe(true)
+
+    expect(error2.begin.row).toBe(2)
+    expect(error2.begin.column).toBe(16)
+    expect(error2.severity).toBe('error')
+    expect(error2.excerpt).toBe("expected ';' at end of declaration [clang-diagnostic-error]")
+    expect(error2.hasFix).toBe(true)
+
+    expect(error3.begin.row).toBe(3)
+    expect(error3.begin.column).toBe(14)
+    expect(error3.severity).toBe('error')
+    expect(error3.excerpt).toBe("expected ';' at end of declaration [clang-diagnostic-error]")
+    expect(error3.hasFix).toBe(true)
+  })
 })
