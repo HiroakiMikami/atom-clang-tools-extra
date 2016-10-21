7fb23cc1eba8464c1c32f395716983249b624bc6
Hiroaki Mikami <hiroaki8270@gmail.com>
2016-10-21 17:02:18 +0900

    Fix Patch implementation
      ref #2

--- spec/patch-spec.js
+++ spec/patch-spec.js
@@ -4,11 +4,8 @@ import Patch from '../lib/patch'
 
 describe('Patch', () => {
   it('apply a change into text editor', () => {
-    const patch = new Patch(0, 3, 'foo')
+    const patch = new Patch(5, 3, 'foo')
 
-    const editor = atom.workspace.buildTextEditor()
-    editor.setText('bar\ntest')
-    patch.apply(editor)
-    expect(editor.getText()).toBe('foo\ntest')
+    expect(patch.apply('bar\n', 5)).toBe('foo\n')
   })
 })
