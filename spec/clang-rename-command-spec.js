5e44d68199d014bf2af8c569bc570e81642457a4
Hiroaki Mikami <hiroaki8270@gmail.com>
2016-10-21 18:03:59 +0900

    Implement clang-rename-command
      ref #3

---
+++ spec/clang-rename-command-spec.js
@@ -0,0 +1,20 @@
+'use babel'
+
+import ClangRenameCommand from '../lib/clang-rename-command'
+
+describe('ClangRenameCommand ', () => {
+  it('invokes the command', () => {
+    const clangRenameCommand = new ClangRenameCommand('./spec/bin/clang-rename')
+
+    let result = null
+    waitsForPromise(() => {
+      return clangRenameCommand.rename([], 0, 'bar', 'foo.cpp').then(output => {
+        result = output
+        return output
+      })
+    })
+    runs(() => {
+      expect(result).toBe(`-offset 0 -new-name bar foo.cpp`)
+    })
+  })
+})
