55afca5370689fe41392e0f2d2bcbd610847d7a6
Hiroaki Mikami <hiroaki8270@gmail.com>
2016-10-21 09:22:27 +0900

    Add the test case of apply-all-fixes
      ref #2

--- spec/clang-tidy-spec.js
+++ spec/clang-tidy-spec.js
@@ -3,7 +3,7 @@
 import ClangTidy from '../lib/clang-tidy'
 
 describe('ClangTidy', () => {
-  it('invoke the command', () => {
+  it('invokes the command', () => {
     const clangTidy = new ClangTidy('./spec/bin/clang-tidy')
 
     let result = null
@@ -21,6 +21,20 @@ describe('ClangTidy', () => {
       expect(result[0].severity).toBe('warning')
       expect(result[0].excerpt).toBe("unused function 'test' [clang-diagnostic-unused-function]")
       expect(result[0].trace.length).toBe(0)
+    })
+  })
+  it('fixes errors in the source code', () => {
+    const clangTidy = new ClangTidy('./spec/bin/clang-tidy-apply-all-fixes')
+
+    let result = null
+    waitsForPromise(() => {
+      return clangTidy.applyAllFixes(['-xcpp'], 'foo.cpp').then(output => {
+        result = output
+        return output
+      })
+    })
+    runs(() => {
+      expect(result).toBe(`-fix-errors -extra-arg -xcpp foo.cpp`)
     })
   })
 })
