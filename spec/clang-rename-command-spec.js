b303540b84df17f7c29bd374fcfa5869f1089c43
Hiroaki Mikami <hiroaki8270@gmail.com>
2016-10-22 15:21:09 +0900

    Fix the implementatin of clang-rename
      ref #9

--- spec/clang-rename-command-spec.js
+++ spec/clang-rename-command-spec.js
@@ -17,7 +17,7 @@ describe('ClangRenameCommand ', () => {
       expect(result).toBe('-offset 0 -new-name bar foo.cpp\n')
     })
   })
-  it('ignores the exit code', () => {
+  it('ignores the exit code if some errors exists', () => {
     const clangRenameCommand = new ClangRenameCommand('./spec/bin/clang-rename-with-nonzero-exit-code')
 
     let result = null
@@ -28,5 +28,22 @@ describe('ClangRenameCommand ', () => {
       })
     })
     runs(() => {})
+  })
+  it('fails when the target symbol is not found.', () => {
+    const clangRenameCommand = new ClangRenameCommand('./spec/bin/clang-rename-symbol-notfound')
+
+    let result = null
+    waitsForPromise(() => {
+      return clangRenameCommand.rename([], 0, 'bar', 'foo.cpp').then(
+        output => null,
+        error => {
+          result = error
+          return error
+        }
+      )
+    })
+    runs(() => {
+      expect(result).toBe('could not find symbol at /tmp/test.cpp:4:7 (offset 30).')
+    })
   })
 })
